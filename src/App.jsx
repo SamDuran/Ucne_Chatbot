9b8be4dbfa7701ffb6dd964e15b138d238515655
jose22gonzalez <urenajosealbeto@gmail.com>
2023-11-20 17:15:16 -0400

    created loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import Loading from './layouts/Loading/Loading '
 
 
 function App() {
@@ -6,7 +7,7 @@ function App() {
 
   return (
     <>
-
+      <Loading />
     </>
   )
 }
