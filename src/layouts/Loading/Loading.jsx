436536357a738931630dd2eabf3b4be8f9380a9b
AndersonGomezM <Andersongomez9870@gmail.com>
2023-11-29 15:54:08 -0400

    Ajustando Loading y añadiendo animaciones

--- src/layouts/Loading/Loading.jsx
+++ src/layouts/Loading/Loading.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import iconbot from "../../assets/Vector.png"
+import iconbot from "../../assets/UcneRobot.svg"
 import "./Loading.css"
 
 const Loading = () => {
@@ -10,9 +10,7 @@ const Loading = () => {
                 style={{ zIndex: "6000" }}
             >
                 <div className='flex-col'>
-
                     <x-loaading className="w-24 h-24">
-
                         <div className="w-[20rem] h-2 mt-[12.50rem] bg-[#D9D9D9] rounded-full overflow-hidden">
                             <div className="h-full bg-[#1b1e65] animate-loading "></div>
                         </div>
@@ -25,27 +23,14 @@ const Loading = () => {
                         <div className='text-[#2E3191] text-[24px] text-center absolute top-[55%] left-1/2 transform -translate-x-1/2'>
                             UCNE CHATBOT
                         </div>
-
-                        {/* <img
-                            src={iconbot}
-                            alt="Icono del bot"
-                            className="absolute w-[6.3rem] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
-                            style={{ zIndex: '10' }}
-                        />
-                        <div className='text-blue-900 text-2xl text-center absolute top-[calc(100% + 1rem)] left-1/2 transform -translate-x-1/2'>
-                            UCNE CHATBOT
-                        </div>
-                        <div className="w-[20rem] h-2 mt-[12.50rem] bg-[#D9D9D9] rounded-full overflow-hidden">
-                            <div className="h-full   bg-[#1b1e65] animate-loading "></div>
-
-                        </div> */}
                     </x-loaading>
-                    <div className="mt-1 text-black font-mono text-[12px] sm:text-[12px] text-end">
-                        Cargando...
+                    <div className="flex mt-1 text-black font-mono text-[12px] sm:text-[12px] justify-end">
+                        Cargando
+                        <div className='h-full flex items-end'>
+                        </div>
+                        <div className="dots ms-1"></div>
                     </div>
-
                 </div>
-
             </div>
         </div>
     )
