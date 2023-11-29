d9ae2cf583d7f7438d9c171946b0bf01d87ef269
AndersonGomezM <Andersongomez9870@gmail.com>
2023-11-29 15:52:40 -0400

    Agregando Loading y rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,51 @@
-import { useState } from 'react'
-import logoUCNE from "./assets/image 11.png"
+import { useState, useEffect } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import logoUCNE from "../src/assets/UcneLogo.png"
 import Loading from './layouts/Loading/Loading'
-import Team from './layouts/Team/Team'
+import Chat from './layouts/Chat/Chat'
+import Team from './layouts/Team/Team';
 
 function App() {
 
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000);
+  }, [])
+
   return (
     <>
-      <div className='fixed flex w-full h-full items-center justify-center overflow-auto'>
-        <img
-          src={logoUCNE}
-          alt="Transparent"
-          className='absolute'
-        />
-        {/*Pantallas*/}
-        <Team/>
+      <div>
+        <div>
+          <div className='fixed flex flex-col w-full h-full items-center justify-center overflow-auto'>
+            <img
+            src={logoUCNE}
+            alt="Transparent"
+            className='absolute'
+            />
+            {isLoading ? <Loading /> : 
+            <Router>
+              <Routes>
+                <Route
+                  exact
+                  path="/Chat"
+                  element={<Chat />}
+                />
+                <Route
+                  path="/Team"
+                  element={<Team />}
+                />
+                <Route
+                  path="*"
+                  element={<Navigate to="/Chat" />}
+                />
+              </Routes>
+            </Router>
+            }
+          </div>
+        </div>
       </div>
     </>
   )
